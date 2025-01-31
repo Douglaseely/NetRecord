e3178a8e6792b37e4603205f5d0d4861bdf4c3e3
FicSci <douglaseelyjr@gmail.com>
2025-01-31 10:42:47 -0600

    Updated the directory utils get solution root to try and use the active product root if it fails

--- src/NetRecord/Utils/DirectoryUtils.cs
+++ src/NetRecord/Utils/DirectoryUtils.cs
@@ -4,15 +4,27 @@ public static class DirectoryUtils
 {
     public static string GetRootPath()
     {
+        // First, try to get the application's base directory
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        // For development environment (looking for .sln file)
         var dir = Directory.GetCurrentDirectory();
         while (dir != null)
         {
             if (Directory.GetFiles(dir, "*.sln").Length > 0)
                 return dir;
 
-            dir = Directory.GetParent(dir)?.FullName;
+            var parentDir = Directory.GetParent(dir);
+            if (parentDir == null)
+                break;
+        
+            dir = parentDir.FullName;
         }
+       
+        if (baseDirectory is null)
+            throw new Exception("Could not find solution root nor active project root directory");
 
-        throw new Exception("Could not find solution root");
+        // If no .sln file found, return the base directory
+        return baseDirectory;
     }
 }
